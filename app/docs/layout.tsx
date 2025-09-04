11ab481177c275830f723fc293fc323bd6665670
Marek Jóźwiak <168720167+66HEX@users.noreply.github.com>
2025-09-04 23:56:39 +0200

    fix: change toaster configuration

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -2,11 +2,14 @@ import { DocsLayout } from "fumadocs-ui/layouts/docs";
 import type { ReactNode } from "react";
 import { baseOptions } from "@/app/layout.config";
 import { source } from "@/lib/source";
+import { Toaster } from "@/app/components/ui/toast";
+
 
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
 		<DocsLayout tree={source.pageTree} {...baseOptions}>
 			{children}
+			<Toaster />
 		</DocsLayout>
 	);
 }
