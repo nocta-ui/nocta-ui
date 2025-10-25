1717e414bfb4ff2f473922afa02e506eb0932618
Marek Jóźwiak <168720167+66HEX@users.noreply.github.com>
2025-10-25 22:09:58 +0200

    fix(docs): fix minor issues

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -23,7 +23,7 @@ import {
 	BreadcrumbPage,
 	BreadcrumbSeparator,
 } from '@/app/components/ui/breadcrumb';
-import { Button } from '@/app/components/ui/button/';
+import { Button } from '@/app/components/ui/button';
 import { Calendar } from '@/app/components/ui/calendar';
 import {
 	Card,
