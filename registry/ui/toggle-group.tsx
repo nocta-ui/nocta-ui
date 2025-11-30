979465000518410d05d36f543bad0d1586a223b8
Marek Jóźwiak <168720167+66HEX@users.noreply.github.com>
2025-11-30 16:04:40 +0100

    style(toggle-group): change focus ring radius

--- registry/ui/toggle-group.tsx
+++ registry/ui/toggle-group.tsx
@@ -33,7 +33,7 @@ const toggleGroupItemVariants = cva(
 		'flex-1 flex items-center justify-center cursor-pointer hover:bg-card-muted',
 		"[&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0",
 		'transition-[background-color,box-shadow] ease-out-quad duration-100',
-		'focus-visible:ring-1 focus-visible:ring-ring/50 focus-visible:ring-offset-1 focus-visible:ring-offset-ring-offset/50 focus-visible:outline-none focus-visible:rounded-md',
+		'focus-visible:ring-1 focus-visible:ring-ring/50 focus-visible:ring-offset-1 focus-visible:ring-offset-ring-offset/50 focus-visible:outline-none',
 		'first:rounded-l-md last:rounded-r-md',
 	],
 	{
